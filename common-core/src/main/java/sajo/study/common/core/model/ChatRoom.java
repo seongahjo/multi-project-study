f5882be0bbc4d3b5294b134fae44d99c2b0dbabc
seongahjo <seongside@gmail.com>
2019-12-29 03:08:47 +0900

    Fix remove jpa dependency

--- common-core/src/main/java/sajo/study/common/core/model/ChatRoom.java
+++ common-core/src/main/java/sajo/study/common/core/model/ChatRoom.java
@@ -2,16 +2,10 @@ package sajo.study.common.core.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.time.LocalDate;
 
-@Entity
 @Data
 public class ChatRoom {
-    @Id
-    @GeneratedValue
     private Long idx;
     private String name;
     private LocalDate createdAt;
