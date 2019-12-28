499955dedc6af38bb048ad7612f392197b692032
seongahjo <seongside@gmail.com>
2019-12-29 03:16:14 +0900

    Add equals, ignore LocalDate to json

--- common-core/src/main/java/sajo/study/common/core/model/ChatRoom.java
+++ common-core/src/main/java/sajo/study/common/core/model/ChatRoom.java
@@ -1,14 +1,21 @@
 package sajo.study.common.core.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 
 @Data
+@EqualsAndHashCode(of={"idx","name"})
 public class ChatRoom {
     private Long idx;
     private String name;
+    @JsonIgnore
     private LocalDate createdAt;
+
+    public ChatRoom() {
+    }
 
     public ChatRoom(String name) {
         this.name = name;
