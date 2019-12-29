c28556fbe03271d0c20c450948fadfd136713bd8
seongahjo <seongside@gmail.com>
2019-12-29 21:41:00 +0900

    Add BaseDTO to reduce DRY code

--- common-core/src/main/java/sajo/study/common/core/dto/ChatRoomDTO.java
+++ common-core/src/main/java/sajo/study/common/core/dto/ChatRoomDTO.java
@@ -1,24 +1,19 @@
 package sajo.study.common.core.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.time.LocalDate;
-
 @Data
-@EqualsAndHashCode(of={"idx","name"})
-public class ChatRoomDTO {
+@EqualsAndHashCode(of = {"idx", "name"}, callSuper = false)
+public class ChatRoomDTO extends BaseDTO {
     private Long idx;
     private String name;
-    @JsonIgnore
-    private LocalDate createdAt;
 
     public ChatRoomDTO() {
     }
 
     public ChatRoomDTO(String name) {
+        super();
         this.name = name;
-        this.createdAt = LocalDate.now();
     }
 }
