3b5dc16631cd33068856ae82573e2be1f48e3f25
seongahjo <seongside@gmail.com>
2019-12-31 00:58:54 +0900

    Add base service to fetch data

---
+++ common-web/src/main/java/sajo/study/common/web/service/BaseService.java
@@ -0,0 +1,27 @@
+package sajo.study.common.web.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sajo.study.common.core.dto.BaseDTO;
+import sajo.study.common.core.model.BaseEntity;
+
+import static sajo.study.common.core.util.ModelMapperUtils.map;
+
+public abstract class BaseService<T extends BaseEntity, U extends BaseDTO> {
+    protected JpaRepository<T, Long> repository;
+    protected Class<T> clazz;
+    protected Class<U> dto;
+
+    public BaseService(JpaRepository<T, Long> repo, Class<T> clazz, Class<U> dto) {
+        this.repository = repo;
+        this.clazz = clazz;
+        this.dto = dto;
+    }
+
+    public U findOne(Long idx) {
+        return map(this.repository.getOne(idx), dto);
+    }
+
+    public T save(T entity) {
+        return this.repository.save(entity);
+    }
+}
