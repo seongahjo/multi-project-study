e64819835a7000c0a8c9cb1a2410ff08a00ce156
seongahjo <seongside@gmail.com>
2019-12-31 19:37:25 +0900

    Add base scan packages to get class from core module

--- common-web/src/main/java/sajo/study/common/web/WebApplication.java
+++ common-web/src/main/java/sajo/study/common/web/WebApplication.java
@@ -3,9 +3,8 @@ package sajo.study.common.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WebApplication {
-
+@SpringBootApplication(scanBasePackages = {"sajo.study.common.core","sajo.study.common.web"})
+public class WebApplication{
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class,args);
     }
