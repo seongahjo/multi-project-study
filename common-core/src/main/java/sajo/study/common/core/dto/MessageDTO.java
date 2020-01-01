5a08d30404fce68ac513080681603fc9a05a6a2f
seongahjo <seongside@gmail.com>
2020-01-02 00:32:21 +0900

    Add MessageDTO to communicate between server and client

---
+++ common-core/src/main/java/sajo/study/common/core/dto/MessageDTO.java
@@ -0,0 +1,15 @@
+package sajo.study.common.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class MessageDTO extends BaseDTO {
+    private String from;
+    private String message;
+}
