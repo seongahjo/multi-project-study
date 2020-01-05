7d4768fb0c6f05c2eff115991bf8d9f141864ea9
seongahjo <seongside@gmail.com>
2020-01-06 01:43:27 +0900

    Add all args constructor for ChatRoomDTO

--- common-core/src/main/java/sajo/study/common/core/dto/ChatRoomDTO.java
+++ common-core/src/main/java/sajo/study/common/core/dto/ChatRoomDTO.java
@@ -1,5 +1,6 @@
 package sajo.study.common.core.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -7,12 +8,13 @@ import lombok.NoArgsConstructor;
 @Data
 @EqualsAndHashCode(of = {"idx", "name"}, callSuper = false)
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoomDTO extends BaseDTO {
-    private Long idx;
-    private String name;
+	private Long idx;
+	private String name;
 
-    public ChatRoomDTO(String name) {
-        super();
-        this.name = name;
-    }
+	public ChatRoomDTO(String name) {
+		super();
+		this.name = name;
+	}
 }
